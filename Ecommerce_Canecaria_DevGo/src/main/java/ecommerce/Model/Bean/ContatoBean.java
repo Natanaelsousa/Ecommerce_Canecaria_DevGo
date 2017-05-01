c7ea2e862bfc67338796385ad5c07cb8135e67df
Sibele Nepomuceno <snepomuceno@indracompany.com>
2017-05-01 20:03:34 -0300

    Corridigo "FaleConosco"

--- Ecommerce_Canecaria_DevGo/src/main/java/ecommerce/Model/Bean/ContatoBean.java
+++ Ecommerce_Canecaria_DevGo/src/main/java/ecommerce/Model/Bean/ContatoBean.java
@@ -8,91 +8,19 @@ package ecommerce.Model.Bean;
 import ecommerce.Model.Dao.ContatoDAO;
 import ecommerce.Model.DaoImplementation.ContatoDAOImpl;
 import ecommerce.Model.MetodosAcessores.Contato;
-import java.io.Serializable;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
 
 /*  @author sibele */
 @ManagedBean(name = "ContatoBean")
-public class ContatoBean implements Serializable{
+public class ContatoBean  {
 
-    private Contato contato = null;
-    private Integer cod_solicitacao;
-    private Integer cod_tipo_solicitacao;
-    private String nome_completo;
-    private String email_contato;
-    private String descricao_contato;
-    private Integer status_solicitacao;
+    private Contato contato = new Contato();
 
     public ContatoBean() {
     }
 
-    public Integer getStatus_solicitacao() {
-        return status_solicitacao;
-    }
-
-    public void setStatus_solicitacao(Integer status_solicitacao) {
-        this.status_solicitacao = status_solicitacao;
-    }
-
-    public Integer getCod_solicitacao() {
-        return cod_solicitacao;
-    }
-
-    public void setCod_solicitacao(Integer cod_solicitacao) {
-        this.cod_solicitacao = cod_solicitacao;
-    }
-
-    public Integer getCod_tipo_solicitacao() {
-        return cod_tipo_solicitacao;
-    }
-
-    public void setCod_tipo_solicitacao(Integer cod_tipo_solicitacao) {
-        this.cod_tipo_solicitacao = cod_tipo_solicitacao;
-    }
-
-    public String getNome_completo() {
-        return nome_completo;
-    }
-
-    public void setNome_completo(String nome_completo) {
-        this.nome_completo = nome_completo;
-    }
-
-    public String getEmail_contato() {
-        return email_contato;
-    }
-
-    public void setEmail_contato(String email_contato) {
-        this.email_contato = email_contato;
-    }
-
-    public String getDescricao_contato() {
-        return descricao_contato;
-    }
-
-    public void setDescricao_contato(String descricao_contato) {
-        this.descricao_contato = descricao_contato;
-    }
-
-    public Contato getContato() throws SQLException {
-        ContatoDAO contatosDao = new ContatoDAOImpl();
-        List<Contato> contatoList = null;
-        if (contato == null) {
-            FacesContext fc = FacesContext.getCurrentInstance();
-            Map<String, String> params = fc.getExternalContext()
-                    .getRequestParameterMap();
-            String cod_tipo_solicitacao = params.get("cod_tipo_solicitacao");
-            if (cod_tipo_solicitacao != null) {
-                contatoList = contatosDao.ListarSolicitacoesDeFila(Integer.parseInt(cod_tipo_solicitacao));
-            } else {
-                contatoList = new ArrayList<Contato>();
-            }
-        }
+    public Contato getContato() {
         return contato;
     }
 
@@ -127,4 +55,21 @@ public class ContatoBean implements Serializable{
         return ListarSolicitacoesContatos;
     }
 
+    //    private Contato contato = null;
+    //    public Contato getContato() throws SQLException {
+//        ContatoDAO contatosDao = new ContatoDAOImpl();
+//        List<Contato> contatoList = null;
+//        if (contato == null) {
+//            FacesContext fc = FacesContext.getCurrentInstance();
+//            Map<String, String> params = fc.getExternalContext()
+//                    .getRequestParameterMap();
+//            String cod_tipo_solicitacao = params.get("cod_tipo_solicitacao");
+//            if (cod_tipo_solicitacao != null) {
+//                contatoList = contatosDao.ListarSolicitacoesDeFila(Integer.parseInt(cod_tipo_solicitacao));
+//            } else {
+//                contatoList = new ArrayList<Contato>();
+//            }
+//        }
+//        return contato;
+//    }
 }
