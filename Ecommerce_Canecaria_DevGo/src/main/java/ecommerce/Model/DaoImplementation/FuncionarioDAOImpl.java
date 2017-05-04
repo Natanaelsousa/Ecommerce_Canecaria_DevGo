a40aeb3d89924f5cee33af30c5fcaa3c3ad157b2
alessandra.vspontes <alessandra.vspontes@CAS554868W07I26.senacsp.edu.br>
2017-05-03 21:50:15 -0300

    Atualização do FuncionarioDAOImpl

--- Ecommerce_Canecaria_DevGo/src/main/java/ecommerce/Model/DaoImplementation/FuncionarioDAOImpl.java
+++ Ecommerce_Canecaria_DevGo/src/main/java/ecommerce/Model/DaoImplementation/FuncionarioDAOImpl.java
@@ -2,6 +2,8 @@ package ecommerce.Model.DaoImplementation;
 
 import ecommerce.Model.MetodosAcessores.Funcionario;
 import ecommerce.Model.Dao.FuncionarioDAO;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /* @author sibele */
@@ -9,7 +11,7 @@ public class FuncionarioDAOImpl extends GenericaDAOImpl implements FuncionarioDA
 
     //Insere os dados do funcionario no banco
     public void CadastrarFuncionario(Funcionario funcionario) throws SQLException {
-        String query = "INSERT INTO funcionario(nome,senha,cpf,email,depto) "
+        String query = "INSERT INTO funcionario(nome,senha,cpf,login,depto) "
                 + "VALUES(?,?,?,?,?)";
         insert(query, funcionario.getNome_funcionario(), funcionario.getSenha_funcionario(),funcionario.getCpf_funcionario(), funcionario.getLogin_funcionario(),funcionario.getDepartamento_funcionario());
 
@@ -19,10 +21,41 @@ public class FuncionarioDAOImpl extends GenericaDAOImpl implements FuncionarioDA
     public void EditarCadastroFuncionario(Funcionario funcionario) throws SQLException {
 
         String query = "UPDATE funcionario "
-                + "SET nome_funcionario = ?,cpf_funcionario = ?,login_funcionario = ?,senha_funcionario = ?"
-                + "WHERE cpf_funcionario = ?";
+                + "SET nome = ?,senha = ?, cpf = ?,login = ?,depto = ? "
+                + "WHERE cpf = ?";
 
-        update(query, funcionario.getNome_funcionario(), funcionario.getCpf_funcionario(), funcionario.getLogin_funcionario(), funcionario.getSenha_funcionario());
+        update(query, funcionario.getCpf_funcionario(),funcionario.getNome_funcionario(), funcionario.getSenha_funcionario(),funcionario.getCpf_funcionario(), funcionario.getLogin_funcionario(),funcionario.getDepartamento_funcionario());
 
     }
+
+    
+
+    public Funcionario findByCPF(String cpf) throws SQLException {
+        String select = "SELECT * FROM FUNCIONARIO WHERE cpf ='"+cpf+"'";
+        Funcionario funcionario = new Funcionario();
+       
+        PreparedStatement stmt
+                = getConnection().prepareStatement(select);
+
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            
+            funcionario.setNome_funcionario(rs.getString("nome"));
+            funcionario.setSenha_funcionario(rs.getString("senha"));
+            funcionario.setCpf_funcionario(rs.getString("cpf"));
+            funcionario.setLogin_funcionario(rs.getString("login"));
+            funcionario.setDepartamento_funcionario(rs.getString("depto"));
+            funcionario.setCod_funcionario(rs.getInt("cod_funcionario"));
+            
+        
+        }
+
+        rs.close();
+        stmt.close();
+        return funcionario;
+    }
+
+   
+ 
 }
