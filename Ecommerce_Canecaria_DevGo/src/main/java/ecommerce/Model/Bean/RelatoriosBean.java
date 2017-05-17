721318174df4b0b4ad939be239668a395d5e7d6c
alessandra2016 <alehvital@gmail.com>
2017-05-17 12:04:15 -0300

    Atualização - Relatorio e Ambiente Funcionario

---
+++ Ecommerce_Canecaria_DevGo/src/main/java/ecommerce/Model/Bean/RelatoriosBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecommerce.Model.Bean;
+
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author Alessandra
+ */
+@ManagedBean(name = "relatoriosBean")
+
+public class RelatoriosBean {
+
+    /**
+     * Creates a new instance of RelatoriosBean
+     */
+    public RelatoriosBean() {
+    }
+    
+}
