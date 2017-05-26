b2924ad3cb3341ca7e3efccdb1e8f9407c19c92c
Fernando Tsuda <fernando@tvod.com.br>
2017-05-26 10:40:50 -0300

    Teste de inclusão no testeBean
    Solução proposta em https://stackoverflow.com/a/2120183 - Ver item 4 da resposta

--- Ecommerce_Canecaria_DevGo/src/main/java/ecommerce/Model/Bean/ProdutoNavigationControllerBean.java
+++ Ecommerce_Canecaria_DevGo/src/main/java/ecommerce/Model/Bean/ProdutoNavigationControllerBean.java
@@ -5,15 +5,10 @@ import ecommerce.Model.DaoImplementation.ProdutoDAOImpl;
 import ecommerce.Model.MetodosAcessores.Produto;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
-import javax.faces.bean.SessionScoped;
-import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
 /**
@@ -23,43 +18,44 @@ import javax.faces.context.FacesContext;
 @ManagedBean(eager = true)
 @RequestScoped
 public class ProdutoNavigationControllerBean implements Serializable {
-    
- private String categoria;
-  
+
+    private String categoria;
+
     private List<Produto> produtos = null;
-   
-   private String getCategoria( FacesContext context ){
-		Map<String, String> attrs = context.getExternalContext().getRequestParameterMap();
-		return (String) attrs.get("cat");
-	}
-   
-      public String setCategoria(){
-		FacesContext context = FacesContext.getCurrentInstance();
-		this.categoria = getCategoria( context );
-		return null;
-	}
-
-   
-    public List<Produto> ListarProdutosPorCategoria() throws Exception {
-           
-         setCategoria();
-        
-        ProdutoDAO produtosDao = new ProdutoDAOImpl();
-
-        List<Produto> ListaProdutos = produtosDao.ListarProdutosCategoria(categoria);
     
-        return ListaProdutos;
+    // Solução proposta no link https://stackoverflow.com/a/2120183 - Ver item 4.
+    // A lista de produtos selecionada precisa estar ativa no momento do clique no botão.
+    @PostConstruct
+    public void init() {
+        setCategoria();
+        ProdutoDAO produtosDao = new ProdutoDAOImpl();
+        produtos = produtosDao.ListarProdutosCategoria(categoria);
+    }
+
+    private String getCategoria(FacesContext context) {
+        Map<String, String> attrs = context.getExternalContext().getRequestParameterMap();
+        return (String) attrs.get("cat");
+    }
+
+    public String setCategoria() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        this.categoria = getCategoria(context);
+        return null;
+    }
+
+    @Deprecated
+    public List<Produto> ListarProdutosPorCategoria() throws Exception {
+        return getProdutos();
     }
 
     public List<Produto> getProdutos() {
-
-        try {
-            return ListarProdutosPorCategoria();
-        } catch (Exception ex) {
-            return null;
-        }
+        return produtos;
     }
     
+    public String getCategoria() {
+        return categoria;
+    }
+
     public List<Integer> getNumeros() {
         List<Integer> listaIds = new ArrayList<Integer>();
         List<Produto> prods = getProdutos();
@@ -70,4 +66,3 @@ public class ProdutoNavigationControllerBean implements Serializable {
     }
 
 }
-
