60cbe6f46eb4986d979267936d3d7f989a92f808
Natanael Santos <>
2017-05-27 14:14:28 -0300

    Criação do arrayList Produtos e metodo para usar para apresentar na tela de carrinho

--- Ecommerce_Canecaria_DevGo/src/main/java/ecommerce/Model/Bean/CarrinhoBean.java
+++ Ecommerce_Canecaria_DevGo/src/main/java/ecommerce/Model/Bean/CarrinhoBean.java
@@ -7,10 +7,13 @@ package ecommerce.Model.Bean;
 
 
 
+import ecommerce.Model.Dao.ProdutoDAO;
+import ecommerce.Model.DaoImplementation.ProdutoDAOImpl;
 import ecommerce.Model.MetodosAcessores.Produto;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.context.FacesContext;
@@ -24,30 +27,38 @@ import javax.faces.context.Flash;
 @SessionScoped
 public class CarrinhoBean implements Serializable {
   
-  List<Produto> Produtos = new ArrayList<>();
+  private List<Integer> idProdutos = new ArrayList<>();
+  private List<Produto> produtos = new ArrayList<>();
+   ProdutoDAO produtosDao = new ProdutoDAOImpl();
 
-  public CarrinhoBean() {
+  public CarrinhoBean() throws SQLException {
+      
+      apresentaProdutos();
   }
   
-  public String adicionar(Produto produto) {
-    this.Produtos.add(produto);
-    Flash flash = FacesContext.getCurrentInstance()
-	    .getExternalContext().getFlash();
-    flash.put("mensagem", "Produto " + produto.getNome_produto()
-	    + " adicionado com sucesso.");
+  public String adicionar(int produto) {
+     this.idProdutos.add(produto);
+     
+  
     return "Produtos";
   }
 
-    public List<Produto> getProdutos() {
-        return Produtos;
+    public List<Integer> getProdutos() {
+        return idProdutos;
+    }
+    public List<Produto> apresentaProdutos() throws SQLException{
+      for (int idProduto : idProdutos) {
+          produtos.add(produtosDao.BuscarProdutoPorID(idProduto));
+      }
+      return produtos;
     }
 
-    public void setProdutos(List<Produto> Produtos) {
-        this.Produtos = Produtos;
+    public void setProdutos(List<Integer> Produtos) {
+        this.idProdutos = Produtos;
     }
   
   public int getQuantidade() {
-    return Produtos.size();
+    return idProdutos.size();
   }
   
  
