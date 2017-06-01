968b974ee1f2a03d22670a3543f5e07a6bfdc5b7
Erik <Erik@Erik-PC.lan>
2017-06-01 00:14:52 -0300

    Listando produtos de acordo com a categoria ainda sem imagem

---
+++ Ecommerce_Canecaria_DevGo/src/main/java/ecommerce/Model/Bean/ProdutoCategoriaBean.java
@@ -0,0 +1,65 @@
+package ecommerce.Model.Bean;
+
+import ecommerce.Model.Dao.ProdutoDAO;
+import ecommerce.Model.DaoImplementation.ProdutoDAOImpl;
+import ecommerce.Model.MetodosAcessores.Produto;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Erik
+ */
+@ManagedBean
+@ViewScoped
+public class ProdutoCategoriaBean {
+
+    public ProdutoCategoriaBean() {
+        
+    }
+    private String categoria;
+    ProdutoDAO produtosDao = new ProdutoDAOImpl();
+    List<Produto> listaProdutos ;
+
+    @PostConstruct
+    public void iniciar() {
+        
+        try {
+            buscaProdutosPorCategoria();
+        } catch (Exception ex) {
+           System.err.println("Erro");
+        }
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+    
+    public List<Produto> getListaProdutos() {
+        return listaProdutos;
+    }
+
+    public void setListaProdutos(List<Produto> listaProdutos) {
+        this.listaProdutos = listaProdutos;
+    }
+   
+             
+     /* Buscar os produtos de acordo com a categoria */
+    public List<Produto> buscaProdutosPorCategoria() throws Exception {
+        
+                 
+        ProdutoDAO Dao = new ProdutoDAOImpl();
+
+        listaProdutos = Dao.ListarProdutosCategoria(categoria);
+    
+        return listaProdutos;
+        
+    }
+    
+}
